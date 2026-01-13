622394c5b8628d75d3b6af2e38c61b2c227be6e2
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-13 16:17:49 +0800

    Update 09

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(startNum, endNum) {
+    // make sure no negative number
+    if (startNum < 0 || endNum < 0 || !Number.isInteger(startNum) || !Number.isInteger(endNum)) {
+        return "ERROR";
+    }
 
+    // make sure endNum is the largest
+    if (endNum < startNum) {
+        [startNum, endNum] = [endNum, startNum]
+    }
+    let total = 0;
+
+    for (let i = endNum; i >= startNum; i--) {
+        total += endNum;
+        endNum--;
+    }
+    return total;
 };
 
 // Do not edit below this line
