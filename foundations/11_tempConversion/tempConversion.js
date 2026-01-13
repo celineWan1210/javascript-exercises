b8c006f4809b16d3b21866627b005b3ddba16d4e
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-13 16:40:40 +0800

    Update 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  celciusTemp = (temp - 32) * 5/9
+  return Math.round(celciusTemp * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  farenheitTemp = (temp * 9/5) + 32;
+  return Math.round(farenheitTemp * 10)/10;
 };
 
 // Do not edit below this line
