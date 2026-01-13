2ee09cba2c5eb070ea76cf4f7405183376f497af
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-13 16:02:26 +0800

    Update notes 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
+const removeFromArray = function(...argv) {
+    // turn argument into array
+    argvArray = Array.from(argv);
+
+    // first argument is the orginal array
+    array = argvArray[0];
+
+    numbersToRemove = argvArray.slice(1);
+
+    // filter out any number that includes in the numberToRemove array
+    return array
+        .filter(number => !numbersToRemove.includes(number));
 };
+
+console.log((removeFromArray([1, 2, 3, 4], 3, 2)))
 
 // Do not edit below this line
 module.exports = removeFromArray;
