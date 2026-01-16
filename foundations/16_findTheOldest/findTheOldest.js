008d36d677479bcb0523417ee7bb55fbced6db77
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-16 16:35:48 +0800

    Update exercise 16

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,6 +1,10 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const sortOldest = people
+    // x ?? y -> return y if x is undefined
+    .sort(function(a,b){return ((b.yearOfDeath ?? new Date().getFullYear()) - b.yearOfBirth) - ((a.yearOfDeath ?? new Date().getFullYear()) - a.yearOfBirth)});
 
-};
+    return sortOldest[0];
+};  
 
 // Do not edit below this line
 module.exports = findTheOldest;
