1c3a6ce9993f0fdd2ab7e4322d9e273173a7d5ab
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-16 16:08:34 +0800

    Update exercise 14

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,20 @@
-const fibonacci = function() {
+const fibonacci = function(input) {
+    // first and second of fibonanci is 1, 1
+    fibonacciArray = [1, 1];
 
+    if (input == 0) {
+        return 0;
+    } else if (input < 0) {
+        return "OOPS";
+    }
+
+    if (input > 2) {
+        for (let i = 2; i < input; i++) {
+            fibonacciArray.push(fibonacciArray[i-1] + fibonacciArray[i-2]);
+        }
+    }
+
+    return fibonacciArray[input-1];
 };
 
 // Do not edit below this line
