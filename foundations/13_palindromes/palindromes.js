db07b4a55bd8f5c27ce48597e5197c276005ef78
celineWan1210 <wanyingxuan1210@gmail.com>
2026-01-16 13:40:42 +0800

    Update exercise 13

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,19 @@
-const palindromes = function () {
+const palindromes = function (stringInput) {
+    const stringArray = Array.from(stringInput);
+    const filteredArray =  stringArray
+    .map(letter => letter.toLowerCase())
+    .filter(letter => /^[a-z0-9]$/.test(letter));
 
+
+    // create a copy then reversed
+    const reversedArray = filteredArray.slice().reverse();
+
+    // return true if two array are same
+    const arrayEqual = filteredArray.every((letter, index) => 
+        reversedArray[index] === letter
+    )
+
+    return arrayEqual ? true : false;
 };
 
 // Do not edit below this line
